753a141bd3622f29c45658be7ee74b53aed230dc
Josh Cummings <hello@joshcummingsdesign.com>
2016-11-22 08:20:28 -0800

    Fixes spacing in bash PHP

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,36 +29,43 @@ echo "UseTLS=YES" >> /etc/ssmtp/ssmtp.conf
 # create the database if it does not exist
 TERM=dumb php -- "$WORDPRESS_DB_HOST" "$WORDPRESS_DB_USER" "$WORDPRESS_DB_PASSWORD" "$WORDPRESS_DB_NAME" <<'EOPHP'
 <?php
-// database might not exist, so let's try creating it (just to be safe)
-$stderr = fopen('php://stderr', 'w');
-// https://codex.wordpress.org/Editing_wp-config.php#MySQL_Alternate_Port
-//   "hostname:port"
-// https://codex.wordpress.org/Editing_wp-config.php#MySQL_Sockets_or_Pipes
-//   "hostname:unix-socket-path"
-list($host, $socket) = explode(':', $argv[1], 2);
-$port = 0;
-if (is_numeric($socket)) {
-$port = (int) $socket;
-$socket = null;
-}
-$maxTries = 10;
-do {
-$mysql = new mysqli($host, $argv[2], $argv[3], '', $port, $socket);
-if ($mysql->connect_error) {
-  fwrite($stderr, "\n" . 'MySQL Connection Error: (' . $mysql->connect_errno . ') ' . $mysql->connect_error . "\n");
-  --$maxTries;
-  if ($maxTries <= 0) {
+
+  // database might not exist, so let's try creating it (just to be safe)
+  $stderr = fopen('php://stderr', 'w');
+
+  // https://codex.wordpress.org/Editing_wp-config.php#MySQL_Alternate_Port
+  //   "hostname:port"
+  // https://codex.wordpress.org/Editing_wp-config.php#MySQL_Sockets_or_Pipes
+  //   "hostname:unix-socket-path"
+  list($host, $socket) = explode(':', $argv[1], 2);
+  $port = 0;
+
+  if (is_numeric($socket)) {
+    $port = (int) $socket;
+    $socket = null;
+  }
+
+  $maxTries = 10;
+  do {
+    $mysql = new mysqli($host, $argv[2], $argv[3], '', $port, $socket);
+
+    if ($mysql->connect_error) {
+      fwrite($stderr, "\n" . 'MySQL Connection Error: (' . $mysql->connect_errno . ') ' . $mysql->connect_error . "\n");
+      --$maxTries;
+      if ($maxTries <= 0) {
+        exit(1);
+      }
+      sleep(3);
+    }
+  } while ($mysql->connect_error);
+
+  if (!$mysql->query('CREATE DATABASE IF NOT EXISTS `' . $mysql->real_escape_string($argv[4]) . '`')) {
+    fwrite($stderr, "\n" . 'MySQL "CREATE DATABASE" Error: ' . $mysql->error . "\n");
+    $mysql->close();
     exit(1);
   }
-  sleep(3);
-}
-} while ($mysql->connect_error);
-if (!$mysql->query('CREATE DATABASE IF NOT EXISTS `' . $mysql->real_escape_string($argv[4]) . '`')) {
-fwrite($stderr, "\n" . 'MySQL "CREATE DATABASE" Error: ' . $mysql->error . "\n");
-$mysql->close();
-exit(1);
-}
-$mysql->close();
+
+  $mysql->close();
 EOPHP
 
 exec "$@"
